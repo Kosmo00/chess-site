e464640bc4c94b8c0a48b1210248020df31ff094
Kosmo00 <vladiotero19101916@gmail.com>
2021-04-14 21:53:56 -0400

    Adding user to posts requests

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,12 +1,21 @@
 const sequelize = require('sequelize')
 const post = require('../models').Post
 const user = require('../models').User
+const comentary = require('../models').Comentary
 
 module.exports = {
     async list(req, res, next) {
         try {
-            const obtained_post = await post.findAll({})
-            return res.status(200).send(obtained_post)
+            const obtained_posts = await post.findAll({})
+            let response = []
+
+            for (let i = 0; i < obtained_posts.length; i++) {
+                response[i] = obtained_posts[i].toJSON()
+                const obtained_user = await obtained_posts[i].getUser()
+                response[i].user = obtained_user
+            }
+
+            return res.status(200).send(response)
         } catch (err) {
             console.log(err)
             return res.status(400).send(err)
@@ -29,11 +38,23 @@ module.exports = {
                 }
             })
 
+            // Finding commentaries data
+
             response = finded_post.toJSON()
-            response.comentaries = await finded_post.getComentaries()
+            finded_comentaries = await finded_post.getComentaries()
+
+            response.comentaries = []
+
+            for (let i = 0; i < finded_comentaries.length; i++) {
+                response.comentaries[i] = finded_comentaries[i].toJSON()
+                const finded_user = await finded_comentaries[i].getUser()
+                response.comentaries[i].user = finded_user
+            }
+
+            // Finding user data
+
             new_user = await finded_post.getUser()
-            new_user = new_user.toJSON()
-            response.user = new_user
+            response.user = new_user.toJSON()
 
             return res.status(200).send(response)
 
